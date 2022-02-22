2d1005be4f1875dfb3eb09c80c48075a9edceffc
eko-develops <ekodevelops@gmail.com>
2022-02-22 13:55:38 -0800

    Add skeleton for sidebar nav

---
+++ src/components/Header.js
@@ -0,0 +1,22 @@
+const Header = () => {
+  return (
+    <header>
+      <div className="logo">Lost Ark Dashboard</div>
+      <nav>
+        <ul>
+          <li>
+            <a href="#">Stone Engraving</a>
+          </li>
+          <li>
+            <a href="#">Top Engravings</a>
+          </li>
+          <li>
+            <a href="#">Builds</a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
